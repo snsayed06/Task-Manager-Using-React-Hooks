a3f28980f2cfeeb2c7a9d86fbc453b7f48548ec5
snsayed06 <snsayed06@gmail.com>
2020-09-25 18:20:23 -0700

    Updates

---
+++ precache-manifest.0582ac617448338aaa156f8ca6b986a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32377d9f99597f63a566482bc961069c",
+    "url": "/Task-Manager-Using-React-Hooks/index.html"
+  },
+  {
+    "revision": "f55e23c99cb8451fd886",
+    "url": "/Task-Manager-Using-React-Hooks/static/css/main.10cb41c7.chunk.css"
+  },
+  {
+    "revision": "a52c874fdf04b3b73bc4",
+    "url": "/Task-Manager-Using-React-Hooks/static/js/2.e27c2b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-Manager-Using-React-Hooks/static/js/2.e27c2b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55e23c99cb8451fd886",
+    "url": "/Task-Manager-Using-React-Hooks/static/js/main.c4cfb4c1.chunk.js"
+  },
+  {
+    "revision": "aadeb4bcf27958481ecf",
+    "url": "/Task-Manager-Using-React-Hooks/static/js/runtime-main.84f03ea7.js"
+  }
+]);
